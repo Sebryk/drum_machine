a245509fa9fb5988926b1824e33e3568aac911e0
Sebryk <106953297+Sebryk@users.noreply.github.com>
2022-09-06 23:23:46 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // JAVASCRIPT WITH JQUERY
 
 let numberOfButtons = $('.pad').length;
-let drumScreen = $('.screen__title');
+let drumScreen = $('.drum-machine__title');
 
 // Mouse clicks on drum pads
 
 $('.pad').on('click', function () {
-  let buttonInnerHTML = this.innerHTML;
+  let buttonInnerHTML = $(this).html();
 
   makeSound(buttonInnerHTML);
 
@@ -28,55 +28,55 @@ function makeSound(key) {
     case 'q':
       let kick = new Audio('sounds/kick.wav');
       kick.play();
-      drumScreen.innerHTML = 'Kick';
+      drumScreen.html('Kick');
       break;
 
     case 'w':
       let snare = new Audio('sounds/snare.wav');
       snare.play();
-      drumScreen.innerHTML = 'Snare';
+      drumScreen.html('Snare');
       break;
 
     case 'e':
       let clap = new Audio('sounds/clap.wav');
       clap.play();
-      drumScreen.innerHTML = 'Clap';
+      drumScreen.html('Clap');
       break;
 
     case 'a':
       let snap = new Audio('sounds/snap.wav');
       snap.play();
-      drumScreen.innerHTML = 'Snap';
+      drumScreen.html('Snap');
       break;
 
     case 's':
       let closedHat = new Audio('sounds/closed-hat.wav');
       closedHat.play();
-      drumScreen.innerHTML = 'Closed Hi-Hat';
+      drumScreen.html('Closed Hi-Hat');
       break;
 
     case 'd':
       let openHat = new Audio('sounds/open-hat.wav');
       openHat.play();
-      drumScreen.innerHTML = 'Open Hi-Hat';
+      drumScreen.html('Open Hi-Hat');
       break;
 
     case 'z':
       let tom1 = new Audio('sounds/tom-1.wav');
       tom1.play();
-      drumScreen.innerHTML = 'Tom 1';
+      drumScreen.html('Tom 1');
       break;
 
     case 'x':
       let tom2 = new Audio('sounds/tom-2.wav');
       tom2.play();
-      drumScreen.innerHTML = 'Tom 2';
+      drumScreen.html('Tom 2');
       break;
 
     case 'c':
       let tom3 = new Audio('sounds/tom-3.wav');
       tom3.play();
-      drumScreen.innerHTML = 'Tom 3';
+      drumScreen.html('Tom 3');
       break;
 
     default:
@@ -87,21 +87,21 @@ function makeSound(key) {
 // Pressed button animation
 
 function pressedButton(button) {
-  $('.' + button).addClass('pressed-pad');
+  $('.' + button).addClass('pad--pressed');
   setTimeout(function () {
-    $('.' + button).removeClass('pressed-pad');
+    $('.' + button).removeClass('pad--pressed');
   }, 100);
 }
 
 // //NATIVE JAVASCRIPT
 
-// let numberOfButtons = document.querySelectorAll(".pad").length;
-// let drumScreen = document.querySelector(".drum-machine__title");
+// let numberOfButtons = document.querySelectorAll('.pad').length;
+// let drumScreen = document.querySelector('.drum-machine__title');
 
 // // Mouse clicks on drum pads
 
 // for (let i = 0; i < numberOfButtons; i++) {
-//   document.querySelectorAll(".pad")[i].addEventListener("click", function () {
+//   document.querySelectorAll('.pad')[i].addEventListener('click', function () {
 //     let buttonInnerHTML = this.innerHTML;
 
 //     makeSound(buttonInnerHTML);
@@ -112,7 +112,7 @@ function pressedButton(button) {
 
 // // Keyboard keypresses
 
-// document.addEventListener("keypress", function (event) {
+// document.addEventListener('keypress', function (event) {
 //   makeSound(event.key);
 
 //   pressedButton(event.key);
@@ -122,58 +122,58 @@ function pressedButton(button) {
 
 // function makeSound(key) {
 //   switch (key) {
-//     case "q":
-//       let kick = new Audio("sounds/kick.wav");
+//     case 'q':
+//       let kick = new Audio('sounds/kick.wav');
 //       kick.play();
-//       drumScreen.innerHTML = "Kick";
+//       drumScreen.innerHTML = 'Kick';
 //       break;
 
-//     case "w":
-//       let snare = new Audio("sounds/snare.wav");
+//     case 'w':
+//       let snare = new Audio('sounds/snare.wav');
 //       snare.play();
-//       drumScreen.innerHTML = "Snare";
+//       drumScreen.innerHTML = 'Snare';
 //       break;
 
-//     case "e":
-//       let clap = new Audio("sounds/clap.wav");
+//     case 'e':
+//       let clap = new Audio('sounds/clap.wav');
 //       clap.play();
-//       drumScreen.innerHTML = "Clap";
+//       drumScreen.innerHTML = 'Clap';
 //       break;
 
-//     case "a":
-//       let snap = new Audio("sounds/snap.wav");
+//     case 'a':
+//       let snap = new Audio('sounds/snap.wav');
 //       snap.play();
-//       drumScreen.innerHTML = "Snap";
+//       drumScreen.innerHTML = 'Snap';
 //       break;
 
-//     case "s":
-//       let closedHat = new Audio("sounds/closed-hat.wav");
+//     case 's':
+//       let closedHat = new Audio('sounds/closed-hat.wav');
 //       closedHat.play();
-//       drumScreen.innerHTML = "Closed Hi-Hat";
+//       drumScreen.innerHTML = 'Closed Hi-Hat';
 //       break;
 
-//     case "d":
-//       let openHat = new Audio("sounds/open-hat.wav");
+//     case 'd':
+//       let openHat = new Audio('sounds/open-hat.wav');
 //       openHat.play();
-//       drumScreen.innerHTML = "Open Hi-Hat";
+//       drumScreen.innerHTML = 'Open Hi-Hat';
 //       break;
 
-//     case "z":
-//       let tom1 = new Audio("sounds/tom-1.wav");
+//     case 'z':
+//       let tom1 = new Audio('sounds/tom-1.wav');
 //       tom1.play();
-//       drumScreen.innerHTML = "Tom 1";
+//       drumScreen.innerHTML = 'Tom 1';
 //       break;
 
-//     case "x":
-//       let tom2 = new Audio("sounds/tom-2.wav");
+//     case 'x':
+//       let tom2 = new Audio('sounds/tom-2.wav');
 //       tom2.play();
-//       drumScreen.innerHTML = "Tom 2";
+//       drumScreen.innerHTML = 'Tom 2';
 //       break;
 
-//     case "c":
-//       let tom3 = new Audio("sounds/tom-3.wav");
+//     case 'c':
+//       let tom3 = new Audio('sounds/tom-3.wav');
 //       tom3.play();
-//       drumScreen.innerHTML = "Tom 3";
+//       drumScreen.innerHTML = 'Tom 3';
 //       break;
 
 //     default:
@@ -184,8 +184,8 @@ function pressedButton(button) {
 // // Pressed button animation
 
 // function pressedButton(button) {
-//   document.querySelector("." + button).classList.add("pressed-pad");
+//   document.querySelector('.' + button).classList.add('pad__pressed');
 //   setTimeout(function () {
-//     document.querySelector("." + button).classList.remove("pressed-pad");
+//     document.querySelector('.' + button).classList.remove('pad__pressed');
 //   }, 100);
 // }
